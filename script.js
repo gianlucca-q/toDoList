556506cf6711a9922e09fb9d3cedf2eef596a7a7
M0nsterBull <70190867+M0nsterBull@users.noreply.github.com>
2022-12-01 15:53:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,58 @@
 let todoList = JSON.parse(localStorage.getItem("todoList"));
 if (todoList === null) {
-  let crearArchivo = [{id: 0, title: "", desc: ""}];
+  let crearArchivo = [{ id: 0, title: "", desc: "" }];
   localStorage.setItem("todoList", JSON.stringify(crearArchivo));
 }
 let id = todoList.length;
-let btnClear = document.getElementById("btnClear")
-let btnNew = document.getElementById("btnNew")
-let contTasks = document.getElementById("contTasks")
+let btnClear = document.getElementById("btnClear");
+let btnNew = document.getElementById("btnNew");
+let contTasks = document.getElementById("contTasks");
 
-const addTable = () => {
-    console.log(todoList)
-    contTasks.innerHTML = ``
-
-        for (let i = 0; i < todoList.length; i++) {
-            contTasks.innerHTML += `
-                <div class="task">
-                    <div class="taskBtn">
-                        <button id="taskState" onclick="deleteTask(${todoList[i].id})"></button>
-                    </div>
-                    <div class="taskInfo">
-                        <h2 id="taskTitle">${todoList[i].title}</h2>
-                        <p id="taskDesc">${todoList[i].desc}</p>
-                    </div>
+const addElement = () => {
+  console.log(todoList);
+  contTasks.innerHTML = ``;
+  for (let i = 0; i < todoList.length; i++) {
+    contTasks.innerHTML += `
+            <div class="task">
+                <div class="taskBtn">
+                    <button id="taskState" onclick="deleteTask(${todoList[i].id})"></button>
                 </div>
-            `
-        }
-} 
+                <div class="taskInfo">
+                    <h2 id="taskTitle">${todoList[i].title}</h2>
+                    <p id="taskDesc">${todoList[i].desc}</p>
+                </div>
+            </div>
+        `;
+  }
+};
 
 const addTasks = (title, desc) => {
-    id++
-    title.focus()
-    todoList.push({id: id, title: title.value, desc: desc.value});
+  id++;
+  title.focus();
+  if (title.value != "") {
+    todoList.push({ id: id, title: title.value, desc: desc.value });
     localStorage.setItem("todoList", JSON.stringify(todoList));
-     addTable();
-}
+    addElement();
+  }
+  title.value = "";
+  desc.value = "";
+};
 
-const deleteTasks = () => {
-    id = 0;
-    todoList = [];
-    localStorage.setItem("todoList", JSON.stringify(todoList));
-    addTable();
-    console.log(todoList)
-}
+const deleteAllTasks = () => {
+  id = 0;
+  todoList = [];
+  localStorage.setItem("todoList", JSON.stringify(todoList));
+  addElement();
+  console.log(todoList);
+};
 
 const deleteTask = (id) => {
-    for (let i = 0; i < todoList.length; i++) {
-        if (id === todoList[i].id) {
-          todoList.splice(i, 1);
-          localStorage.setItem("todoList", JSON.stringify(todoList));
-          addTable();
-        }
-      }
-}
-addTable()
+  for (let i = 0; i < todoList.length; i++) {
+    if (id === todoList[i].id) {
+      todoList.splice(i, 1);
+      localStorage.setItem("todoList", JSON.stringify(todoList));
+      addElement();
+    }
+  }
+};
+addElement();
